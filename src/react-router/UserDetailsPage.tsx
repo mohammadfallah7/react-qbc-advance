82ae921ed5165979ef60242df264a1de817eaf8b
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-06-29 19:57:07 +0330

    feat: :sparkles: Getting page data

--- src/react-router/UserDetailsPage.tsx
+++ src/react-router/UserDetailsPage.tsx
@@ -1,5 +1,12 @@
+import { useParams, useSearchParams } from "react-router";
+
 const UserDetailPage = () => {
-  return <p>User</p>;
+  const { id } = useParams();
+  const [searchParams] = useSearchParams();
+
+  console.log(searchParams.get("age"));
+
+  return <p>User {id}</p>;
 };
 
 export default UserDetailPage;
