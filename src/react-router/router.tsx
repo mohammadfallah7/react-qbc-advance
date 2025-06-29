f09bfcb1c2a89142900bc58c7ece80d9da5fc062
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-06-29 19:46:35 +0330

    feat: :sparkles: Implement dynamic routes

--- src/react-router/router.tsx
+++ src/react-router/router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router";
 import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
 import ContactPage from "./ContactPage";
+import UserDetailPage from "./UserDetailsPage";
 
 const router = createBrowserRouter([
   { path: "/", Component: HomePage },
   { path: "/users", Component: UserListPage },
   { path: "/contact", Component: ContactPage },
+  { path: "/users/:id", Component: UserDetailPage },
 ]);
 
 export default router;
