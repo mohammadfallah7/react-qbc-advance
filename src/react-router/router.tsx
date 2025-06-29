f3e7f6c5f22ec76bee1a93cc4c5ef669575b5958
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-06-29 19:29:50 +0330

    feat: :sparkles: Define some routes

--- src/react-router/router.tsx
+++ src/react-router/router.tsx
@@ -1,5 +1,12 @@
 import { createBrowserRouter } from "react-router";
+import HomePage from "./HomePage";
+import UserListPage from "./UserListPage";
+import ContactPage from "./ContactPage";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  { path: "/", Component: HomePage },
+  { path: "/users", Component: UserListPage },
+  { path: "/contact", Component: ContactPage },
+]);
 
 export default router;
