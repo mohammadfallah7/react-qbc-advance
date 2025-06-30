353aba07ff7d4a5bec74f6dbc2f2f3279638481e
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-06-30 19:18:21 +0330

    feat: :sparkles: Implement nested route

--- src/react-router/router.tsx
+++ src/react-router/router.tsx
@@ -3,12 +3,19 @@ import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
 import ContactPage from "./ContactPage";
 import UserDetailPage from "./UserDetailsPage";
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
-  { path: "/", Component: HomePage },
-  { path: "/users", Component: UserListPage },
-  { path: "/contact", Component: ContactPage },
-  { path: "/users/:id", Component: UserDetailPage },
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      { index: true, Component: HomePage },
+      { path: "users", Component: UserListPage },
+      { path: "contact", Component: ContactPage },
+      { path: "users/:id", Component: UserDetailPage },
+    ],
+  },
 ]);
 
 export default router;
